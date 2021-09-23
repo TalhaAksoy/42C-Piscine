a429dd5da24f8e728173a38f83e7c850617ed164
Talha Aksoy <56833887+TalhaAksoy@users.noreply.github.com>
2021-09-23 16:08:50 +0300

    Piscine C-04 (%100)

---
+++ c-04-85/ex02/ft_putnbr.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void	ft_putchar(char x)
+{
+	write(1, &x, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+			ft_putnbr(nb / 10);
+		ft_putchar(48 + nb % 10);
+	}
+}
---
+++ c-04-85/ex03/ft_atoi.c
@@ -0,0 +1,24 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	s;
+	int	res;
+
+	i = 0;
+	s = 1;
+	res = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			s *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (str[i] - '0') + (res * 10);
+		i++;
+	}
+	return (res * s);
+}
